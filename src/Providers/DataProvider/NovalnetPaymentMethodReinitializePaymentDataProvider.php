d42f55f15789231d259c2ac8b60c6b2b12de702b
abirajs <125948976+abirajs@users.noreply.github.com>
2023-04-11 19:13:20 +0530

    Update NovalnetPaymentMethodReinitializePaymentDataProvider.php

--- src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentDataProvider.php
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentDataProvider.php
@@ -9,127 +9,48 @@
 namespace Novalnet\Providers\DataProvider;
 
 use Plenty\Plugin\Templates\Twig;
-use Novalnet\Services\PaymentService;
-use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
-use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
-use Novalnet\Helper\PaymentHelper;
-use Novalnet\Services\SettingsService;
-use Plenty\Modules\Helper\Services\WebstoreHelper;
+use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
+use Plenty\Modules\Payment\Method\Models\PaymentMethod;
 
 /**
- * Class NovalnetPaymentMethodReinitializePaymentDataProvider
+ * Class NovalnetPaymentMethodScriptDataProvider
  *
  * @package Novalnet\Providers\DataProvider
  */
-class NovalnetPaymentMethodReinitializePaymentDataProvider
+class NovalnetPaymentMethodScriptDataProvider
 {
     /**
-     * Display the reinitiate payment button
+     * Script for displaying the reinitiate payment button
      *
      * @param Twig $twig
-     * @param Arguments $arg
      *
      * @return string
      */
-    public function call(Twig $twig, WebstoreHelper $webstoreHelper, $arg)
+    public function call(Twig $twig)
     {
-        $order = $arg[0];
-        $paymentService     = pluginApp(PaymentService::class);
-        $basketRepository   = pluginApp(BasketRepositoryContract::class);
-        $sessionStorage     = pluginApp(FrontendSessionStorageFactoryContract::class);
-        $paymentHelper      = pluginApp(PaymentHelper::class);
-        $settingsService    = pluginApp(SettingsService::class);
-        // Get the Novalnet payment method Id
-        foreach($order['properties'] as $orderProperty) {
-            if($orderProperty['typeId'] == 3)
-            {
-                $mopId = $orderProperty['value'];
+        // Load the all Novalnet payment methods
+        $paymentMethodRepository = pluginApp(PaymentMethodRepositoryContract::class);
+        $paymentMethods          = $paymentMethodRepository->allForPlugin('plenty_novalnet');
+        if(!is_null($paymentMethods)) {
+            $paymentMethodIds              = [];
+            $nnPaymentMethodKey = $nnPaymentMethodId = '';
+            foreach($paymentMethods as $paymentMethod) {
+				if (strpos($paymentMethod->paymentKey, 'NOVALNET') !== false) {
+					if($paymentMethod instanceof PaymentMethod) {
+						$paymentMethodIds[] = $paymentMethod->id;
+						if($paymentMethod->paymentKey == 'NOVALNET_APPLEPAY') {
+							$nnPaymentMethodKey = $paymentMethod->paymentKey;
+							$nnPaymentMethodId = $paymentMethod->id;
+						}
+					}
+				}
             }
         }
-        // Get the Novalnet payment key and MOP Id
-        $transactionDetails = $paymentService->getDetailsFromPaymentProperty($order['id']);
-        // Get the payment method key
-        $paymentKey = $paymentHelper->getPaymentKeyByMop($mopId);
-        // Build the payment request paramters
-        if(!empty($basketRepository->load())) {
-            // Assign the billing and shipping address Id
-            $basketRepository->load()->customerInvoiceAddressId = !empty($basketRepository->load()->customerInvoiceAddressId) ? $basketRepository->load()->customerInvoiceAddressId : $order['billingAddress']['id'];
-            $basketRepository->load()->customerShippingAddressId = !empty($basketRepository->load()->customerShippingAddressId) ? $basketRepository->load()->customerShippingAddressId : $order['deliveryAddress']['id'];
-
-            // Get the proper order amount even the system currency and payment currency are differ
-            if(count($order['amounts']) > 1) {
-                foreach($order['amounts'] as $orderAmount) {
-                    if($basketRepository->load()->currency == $orderAmount['currency']) {
-                        $invoiceAmount = $paymentHelper->convertAmountToSmallerUnit($orderAmount['invoiceTotal']);
-                    }
-                }
-            } else {
-                $invoiceAmount = $paymentHelper->convertAmountToSmallerUnit($order['amounts'][0]['invoiceTotal']);
-            }
-
-            // Set the required values into session
-            $sessionStorage->getPlugin()->setValue('nnOrderNo', $order['id']);
-            $sessionStorage->getPlugin()->setValue('nnOrderCreator', $order['ownerId']);
-            $sessionStorage->getPlugin()->setValue('nnBillingAddressId', $order['billingAddress']['id']);
-            $sessionStorage->getPlugin()->setValue('nnShippingAddressId', $order['deliveryAddress']['id']);
-            $sessionStorage->getPlugin()->setValue('nnOrderAmount', $invoiceAmount);
-            $sessionStorage->getPlugin()->setValue('paymentkey', $paymentKey);
-            $sessionStorage->getPlugin()->setValue('mop', $mopId);
-
-            // Build the payment request parameters
-            $paymentRequestData = $paymentService->generatePaymentParams($basketRepository->load(), $paymentKey, $invoiceAmount);
-
-            // Set the payment request parameters into session
-            $sessionStorage->getPlugin()->setValue('nnPaymentData', $paymentRequestData);
-
-            // Get the Credit card form loading parameters
-            if ($paymentKey == 'NOVALNET_CC') {
-                 $ccFormDetails = $paymentService->getCreditCardAuthenticationCallData($basketRepository->load(), strtolower($paymentKey), $invoiceAmount);
-                 $ccCustomFields = $paymentService->getCcFormFields();
-            }
-            // Get the required details for Google Pay payment
-            if($paymentKey == 'NOVALNET_GOOGLEPAY') {
-                // Get the seller name from the shop configuaration
-                $sellerName = $settingsService->getPaymentSettingsValue('business_name', 'novalnet_googlepay');
-                $googlePayData = [
-                                    'clientKey'           => trim($settingsService->getPaymentSettingsValue('novalnet_client_key')),
-                                    'merchantId'          => $settingsService->getPaymentSettingsValue('payment_active', 'novalnet_googlepay'),
-                                    'sellerName'          => !empty($sellerName) ? $sellerName : $webstoreHelper->getCurrentWebstoreConfiguration()->name,
-                                    'enforce'             => $settingsService->getPaymentSettingsValue('enforce', 'novalnet_googlepay'),
-                                    'buttonType'          => $settingsService->getPaymentSettingsValue('button_type', 'novalnet_googlepay'),
-                                    'buttonTheme'         => $settingsService->getPaymentSettingsValue('button_theme', 'novalnet_googlepay'),
-                                    'buttonHeight'        => $settingsService->getPaymentSettingsValue('button_height', 'novalnet_googlepay'),
-                                    'testMode'            => ($settingsService->getPaymentSettingsValue('test_mode', 'novalnet_googlepay') == true) ? 'SANDBOX' : 'PRODUCTION'
-                                 ];
-             }
-
-            // Check if the birthday field needs to show for guaranteed payments
-            $showBirthday = ((!isset($paymentRequestData['paymentRequestData']['customer']['billing']['company']) && !isset($paymentRequestData['paymentRequestData']['customer']['birth_date'])) || (isset($paymentRequestData['paymentRequestData']['customer']['birth_date']) && time() < strtotime('+18 years', strtotime($paymentRequestData['paymentRequestData']['customer']['birth_date'])))) ? true : false;
-          }
-
-        // If the Novalnet payments are rejected do the reinitialize payment
-        if(strpos($paymentKey, 'NOVALNET') !== false && ((!empty($transactionDetails['tx_status']) && !in_array($transactionDetails['tx_status'], ['PENDING', 'ON_HOLD', 'CONFIRMED', 'DEACTIVATED'])) || empty($transactionDetails['tx_status']))) {
-            return $twig->render('Novalnet::NovalnetPaymentMethodReinitializePaymentDataProvider',
-                                        [
-                                            'order' => $order,
-                                            'paymentMethodId' => $mopId,
-                                            'paymentMopKey' => $paymentKey,
-                                            'reinitializePayment' => 1,
-                                            'nnPaymentProcessUrl' => $paymentService->getProcessPaymentUrl(),
-                                            'paymentName' => $paymentHelper->getCustomizedTranslatedText('template_' . strtolower($paymentKey)),
-                                            'transactionData' => !empty($ccFormDetails) ? $ccFormDetails : '',
-                                            'customData' => !empty($ccCustomFields) ? $ccCustomFields : '',
-                                            'showBirthday' => $showBirthday,
-                                            'orderAmount' => $invoiceAmount,
-                                            'redirectPayment' => $paymentService->isRedirectPayment($paymentKey),
-                                            'redirectUrl' => $paymentService->getRedirectPaymentUrl(),
-                                            'orderLang'   => $paymentRequestData['paymentRequestData']['custom']['lang'],
-                                            'countryCode' => $paymentRequestData['paymentRequestData']['customer']['billing']['country_code'],
-                                            'orderCurrency'  => $basketRepository->load()->currency,
-                                            'googlePayData' => !empty($googlePayData) ? $googlePayData : ''
-                                        ]);
-        } else {
-            return '';
-        }
+        return $twig->render('Novalnet::NovalnetPaymentMethodScriptDataProvider',
+                                    [
+                                        'paymentMethodIds'      => $paymentMethodIds,
+                                        'nnPaymentMethodKey'    => $nnPaymentMethodKey,
+                                        'nnPaymentMethodId'     => $nnPaymentMethodId
+                                    ]);
     }
 }
