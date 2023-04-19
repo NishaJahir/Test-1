a07ba48adbacd127dc6fe20402fbecede05b0235
abirajs <125948976+abirajs@users.noreply.github.com>
2023-04-19 14:14:48 +0530

    Update NovalnetPaymentMethodReinitializePaymentDataProvider.php

--- src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentDataProvider.php
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentDataProvider.php
@@ -102,7 +102,6 @@ class NovalnetPaymentMethodReinitializePaymentDataProvider
                                         'sellerName'          => !empty($sellerName) ? $sellerName : $webstoreHelper->getCurrentWebstoreConfiguration()->name,
                                         'enforce'             => $settingsService->getPaymentSettingsValue('enforce', 'novalnet_googlepay'),
                                         'buttonType'          => $settingsService->getPaymentSettingsValue('button_type', 'novalnet_googlepay'),
-                                        'buttonTheme'         => $settingsService->getPaymentSettingsValue('button_theme', 'novalnet_googlepay'),
                                         'buttonHeight'        => $settingsService->getPaymentSettingsValue('button_height', 'novalnet_googlepay'),
                                         'testMode'            => ($settingsService->getPaymentSettingsValue('test_mode', 'novalnet_googlepay') == true) ? 'SANDBOX' : 'PRODUCTION'
                                      ];
